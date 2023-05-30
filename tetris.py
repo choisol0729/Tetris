ec8c2e01bf3ff6b3ede4ca79a0e356bc121ea905
choisol0729 <choisol0729@gmail.com>
2023-05-30 22:12:32 +0900

    block detection + block differentiation

--- tetris.py
+++ tetris.py
@@ -6,29 +6,30 @@ blocks = ["I", "J", "L", "O", "S", "T", "Z"]
 blocksShape = {"I":[[0,1,0,0],
                 [0,1,0,0],
                 [0,1,0,0],
-                [0,1,0,0]],"J": [[0,1,0],
-                            [0,1,0],
-                            [1,1,0]],"L": [[1,0,0],
-                                        [1,0,0],
-                                        [1,1,0]],"O": [[1,1],
-                                                    [1,1]],"S": [[0,1,1],
-                                                                [1,1,0],
-                                                                [0,0,0]],"T": [[0,1,0],
-                                                                            [1,1,1],
-                                                                            [0,0,0]],"Z": [[1,1,0],
-                                                                                        [0,1,1],
+                [0,1,0,0]],"J": [[0,2,0],
+                            [0,2,0],
+                            [2,2,0]],"L": [[3,0,0],
+                                        [3,0,0],
+                                        [3,3,0]],"O": [[4,4],
+                                                    [4,4]],"S": [[0,5,5],
+                                                                [5,5,0],
+                                                                [0,0,0]],"T": [[0,6,0],
+                                                                            [6,6,6],
+                                                                            [0,0,0]],"Z": [[7,7,0],
+                                                                                        [0,7,7],
                                                                                         [0,0,0]]}
-block = ""
+block = None
 
 class block:
     def __init__(self, name):
+        self.name = name
         self.matrix = blocksShape[name]
         self.pos = [0,4]
 
 def turnover(board):
     global block
-    if block == "": 
-        block = blocks[random.randint(0,6)]
+    if block == None: 
+        block = block(blocks[random.randint(0,6)])
         # place the block on top of the board
         print("Block:", block)
         spawnBlock(board, block)
@@ -39,9 +40,9 @@ def turnover(board):
     
     if (cmd.lower() == "down") or (cmd.lower() == "left") or (cmd.lower() == "right"):
         print("Move cmd: ", cmd.lower())
-        move(cmd.lower())
+        move(cmd.lower(), block)
     elif cmd == "":
-        move("down")
+        move("down", block)
     else:
         # other command operation (rotate, instant drop)
         if cmd.lower() == "drop":
@@ -73,22 +74,21 @@ def drop(block):
     pass
 
 def detectBlock(blockName):
-    
+    pass
 
 def spawnBlock(board, block):
-    actBlock = blocksShape[block]
-    if actBlock != blocksShape["I"] and actBlock != blocksShape["O"]:
+    if block.matrix != blocksShape["I"] and block.matrix != blocksShape["O"]:
         for i in range(3):
             for j in range(3):
-                board[i][j+4] = actBlock[i][j]
-    elif actBlock == blocksShape["I"]:
+                board[i][j+4] = block.matrix[i][j]
+    elif block.matrix == blocksShape["I"]:
         for i in range(4):
             for j in range(4):
-                board[i][j+4] = actBlock[i][j]
-    elif actBlock == blocksShape["O"]:
+                board[i][j+4] = block.matrix[i][j]
+    elif block.matrix == blocksShape["O"]:
         for i in range(2):
             for j in range(2):
-                board[i][j+4] = actBlock[i][j]
+                board[i][j+4] = block.matrix[i][j]
     pass
 
 # Game Start
